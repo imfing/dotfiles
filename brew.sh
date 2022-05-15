e582bf343ce45b621dbdbf024378c8b2f6632784
Fing <fuxin1997@gmail.com>
2022-05-15 16:31:38 -0400

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Install command line tools via Homebrew
+
+brew update
+
+# https://github.com/sharkdp/bat
+brew install bat
+
+# https://github.com/sharkdp/fd
+brew install fd
+
+# https://github.com/junegunn/fzf
+brew install fzf
+
+# https://github.com/neovim/neovim
+brew install neovim
+
+# https://github.com/ogham/exa
+brew install exa
+
